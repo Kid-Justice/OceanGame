16993627caad7dc42bf3e997ef47f534b9f4f515
Kid-Justice <drew.jennings@outlook.com>
2020-05-26 16:33:36 -0700

    landmine script

---
+++ Pier Pressure/Assets/Scripts/Landmine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Landmine : MonoBehaviour
+{
+    public int damage = 10;
+    public GameObject ExplosionEffect;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Health>() != null)
+        {
+            //collision.GetComponent<Health>().health -= damage;
+            GameObject Explode = Instantiate(ExplosionEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
