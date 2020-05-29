7c7456036f230e50a60b96886b772f9837403e93
realGingerBreadMan <48932947+realGingerBreadMan@users.noreply.github.com>
2020-05-28 18:45:22 -0700

    HighScore created

--- Pier Pressure/Assets/Scripts/EndScore.cs
+++ Pier Pressure/Assets/Scripts/EndScore.cs
@@ -6,12 +6,27 @@ using UnityEngine.UI;
 public class EndScore : MonoBehaviour
 {
     private Text scoreText; 
-    int score; 
+    private Text highScoreText; 
+    int currScore; 
+    int highScore = 0; 
+    GameObject[] ObjWithTag;
+    GameObject[] tagObj; 
     // Start is called before the first frame update
     void Start()
-    {
-         score = PlayerPrefs.GetInt("score", 100);
-         
+    {    
+        //score 
+        currScore = PlayerPrefs.GetInt("score", 100);
+        ObjWithTag = GameObject.FindGameObjectsWithTag("Score");
+        scoreText = ObjWithTag[0].GetComponent<Text>();
+        scoreText.text = " " + currScore; 
+        //highscore
+        tagObj = GameObject.FindGameObjectsWithTag("HighScore");
+        highScoreText = tagObj[0].GetComponent<Text>();
+        if(currScore > highScore)   
+        {
+            highScore = currScore;   
+		}
+        highScoreText.text = " " + highScore; 
     }
     // Update is called once per frame
     void Update()
