bff7f2f19ffe7e46ac356e0a795ce1b47c2afefe
realGingerBreadMan <48932947+realGingerBreadMan@users.noreply.github.com>
2020-06-02 16:31:17 -0700

    added menu music script

---
+++ Pier Pressure/Assets/Scripts/MenuMusicScipt.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuMusicScipt : MonoBehaviour
+{
+    int i = 0; 
+    string sceneName; 
+    GameObject[] SoundObject; 
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        DontDestroyOnLoad(this.gameObject);   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SoundObject = GameObject.FindGameObjectsWithTag("MMusic"); 
+    
+        Scene currentScene = SceneManager.GetActiveScene ();
+        string sceneName = currentScene.name;
+
+        if(sceneName == "Game")
+        {
+              Destroy(this.gameObject); 
+		}
+        if(SoundObject.Length > 1)
+        {
+            for(int z = 1; z < SoundObject.Length; z++)
+            {
+                Destroy(SoundObject[i]);
+			}
+		}
+    }
+}
