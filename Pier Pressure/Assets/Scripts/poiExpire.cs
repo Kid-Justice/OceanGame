49003d6756e6a70dfb77929873d921a40271630c
realGingerBreadMan <48932947+realGingerBreadMan@users.noreply.github.com>
2020-06-02 17:45:25 -0700

    sonar ping update

--- Pier Pressure/Assets/Scripts/poiExpire.cs
+++ Pier Pressure/Assets/Scripts/poiExpire.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class poiExpire : MonoBehaviour
 {
+    public GameObject player;   
+    int distance = 25;      
+
     public GameObject scanTreasure; 
     float scanTimer = 0f; 
     float timeTillScan = 5f;
+    //audio
+    public GameObject SonarSound; 
+    public float spawnTimer = 0f;
+    float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player"); 
+        //pOI = GameObject.FindGameObjectsWithTag("POI");
     }
 
     // Update is called once per frame
@@ -25,5 +33,16 @@ public class poiExpire : MonoBehaviour
             PlayerPrefs.SetFloat("scanTimer", 0f);
             Destroy(gameObject);   
 		}
+        //if player is close to POI, play sonar ping SFX at an interval
+        if(Vector3.Distance(transform.position, player.transform.position) < distance)
+        {
+           timer += Time.deltaTime; 
+		}
+        if(timer >= 4)
+        {
+            Instantiate(SonarSound, transform.position, Quaternion.identity);   
+            timer = 0f; 
+		}
     }
+  
 }
